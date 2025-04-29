87b77313bfd61ac8e9bcb6577cbaff103721c640
Kai Wui Ye <kye5595@gmail.com>
2025-04-29 19:35:24 -0400

    table header does not repeat for each row

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function makeFreelancerQuote() {
   const freelancerName = NAMES[Math.floor(Math.random() * NAMES.length)];
   const freelancerOccupations =
     OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
-  const price = Math.floor(Math.random() * 181) + 20;
-  return { freelancerName, freelancerOccupations, price };
+  const rate = Math.floor(Math.random() * 181) + 20;
+  return { freelancerName, freelancerOccupations, rate };
 }
 
 const freelancerTag = Array.from(
@@ -28,25 +28,55 @@ console.log(freelancerTag);
 
 // Average Rate
 function getAverageRate() {
-  const justPrice = freelancerTag.map(({ price }) => price);
+  const justPrice = freelancerTag.map(({ rate }) => rate);
   const sumPrice = Object.values(justPrice).reduce((acc, curr) => acc + curr);
   return sumPrice / NUM_FREELANCERS;
 }
 console.log(getAverageRate());
 
-// Single Row
-function freelancerRow(row) {
-    const rowContainer = document.createElement("table");
-    rowContainer.classList.add("row");
-    // const content = 
-}
+// Full Table
+function freelancerTable() {
+  const article = document.createElement("article");
+  article.classList.add("table-container");
 
+  const table = document.createElement("table");
+  table.classList.add("rates");
+
+  const tableHeader = `
+    <thead>
+        <tr>    
+            <th>Name</th>
+            <th>Occupation</th>
+            <th>Rate</th>
+        </tr>
+    </thead>
+    <tbody></tbody>`;
+  table.innerHTML = tableHeader;
+
+  const tbody = table.querySelector("tbody");
+
+  freelancerTag.forEach((freelancer) => {
+    const row = document.createElement("tr");
+    const content = `
+        <td>${freelancer.freelancerName}</td>
+        <td>${freelancer.freelancerOccupations}</td>
+        <td>$${freelancer.rate}</td>`;
+    row.innerHTML = content;
+    tbody.appendChild(row);
+  });
+  article.appendChild(table);
+  return article;
+}
 
 // Render
 function render() {
   const $app = document.querySelector("#app");
-  $app.innerHTML = `<h1>Freelancer Forum</h1><p>The average rate is $${getAverageRate(
-    freelancerTag
-  )}</p>`;
+  $app.innerHTML = `
+  <h1>Freelancer Forum</h1>
+  <p>The average rate is $${getAverageRate(freelancerTag)}</p>`;
+
+  const table = freelancerTable(freelancerTag);
+  $app.appendChild(table);
 }
+
 render();
